dff1f053f634275e0a000ed75d267d15507696d6
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-27 12:42:28 +0500

    up

---
+++ main-3.js
@@ -0,0 +1,16 @@
+let arr = ["olma", "nok"];
+arr.push("banan", "anjir", "kivi")
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.unshift("tarvuz", "olcha");
+console.log(arr);
+arr.shift()
+console.log(arr.length);
+// ------------------------------------
+let arr2 = ["oq", "qora", "qizil"]
+arr2.push("sariq", "yashil")
+console.log(arr2);
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]);
+}
