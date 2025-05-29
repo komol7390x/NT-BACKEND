5c748babfcd4df300baae2e1386a2d5301a326a0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-29 11:13:36 +0500

    up

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-calc = (a, b) => ({ A: n => a * n, B:n=>b*n})
-const { A: x, B: y } = calc(4, 3)
-console.log(x(3)+y(2)) //18
+let calc = (a, b) => ({ A: n => a * n, B: n => b * n });
+const { A: x, B: y } = calc(4, 3);
+console.log(x(3) + y(2)); //18
