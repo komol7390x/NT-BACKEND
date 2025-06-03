285fc85576337ec694efa4cb53f835be5072fa93
komol7390x <www.komol8689@gmail.com>
2025-06-03 15:42:04 +0500

    up

---
+++ main-12_1.js
@@ -0,0 +1,4 @@
+fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then(response => response.json())
+    .then(data => console.log("User:", data))
+    .catch(error => console.log("Xatolik:", error));
